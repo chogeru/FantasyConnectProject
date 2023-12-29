847b85cce35da870268578a8444c0071f7184b79
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2023-12-29 22:26:28 +0900

    近接攻撃のパターン追加

--- FantasyConnect/Assets/開発用リソース/Scripts/Wepon/MeleeWeponAttckCol.cs
+++ FantasyConnect/Assets/開発用リソース/Scripts/Wepon/MeleeWeponAttckCol.cs
@@ -6,15 +6,29 @@ public class MeleeWeponAttckCol : MonoBehaviour
 {
     [SerializeField]
     private int Damage = 80;
+    [SerializeField, Header("çUåÇSE")]
+    private List<AudioClip> m_AttackSEClip;
+    [SerializeField]
+    private AudioSource m_AttckSE;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
             EnemySystem enemySystem = other.GetComponent<EnemySystem>();
-
+            AttckSE();
             enemySystem.TakeDamage(Damage);
 
         }
     }
+    private void AttckSE()
+    {
+        if (m_AttckSE != null && m_AttackSEClip.Count > 0)
+        {
+            int seRandamIndex = Random.Range(0, m_AttackSEClip.Count);
+            AudioClip selectedSE = m_AttackSEClip[seRandamIndex];
 
+            m_AttckSE.clip = selectedSE;
+            m_AttckSE.Play();
+        }
+    }
 }
--- FantasyConnect/Assets/開発用リソース/Scripts/Wepon/PlayerMeleeWepon.cs
+++ FantasyConnect/Assets/開発用リソース/Scripts/Wepon/PlayerMeleeWepon.cs
@@ -7,10 +7,21 @@ public class PlayerMeleeWepon : MonoBehaviour
 {
     [SerializeField, Header("攻撃用コライダー")]
     private GameObject m_AttckCol;
+    [SerializeField, Header("パワーアタックコライダー")]
+    private GameObject m_PAAttackCol;
     [SerializeField, Header("プレイヤーのアニメーター")]
     private Animator m_PlayerAnimator;
-    [SerializeField, Header("トレイル")]
+    [SerializeField, Header("通常攻撃トレイル")]
     private GameObject m_AttackTrail;
+    [SerializeField,Header("パワーアタックトレイル")]
+    private GameObject m_PWAtackTrail;
+
+    [SerializeField, Header("攻撃ボイス")]
+    private List<AudioClip> m_ATVoices;
+  
+    [SerializeField, Header("ボイス")]
+    private AudioSource m_Voice;
+  
     private void Update()
     {
         PlayerSystem playerSystem = GetComponentInParent<PlayerSystem>();
@@ -29,19 +40,36 @@ public class PlayerMeleeWepon : MonoBehaviour
             MeleeAttckEnd();
             playerSystem.isAttck = false;
             playerSystem.isStrongAttck = false;
+            playerSystem.isWeponChange = true;
+
         }
     }
     private void MelleAttck()
     {
-        m_AttckCol.SetActive(true);
-        m_AttackTrail.SetActive(true);
+        PlayerSystem playerSystem = GetComponentInParent<PlayerSystem>();
+        AttckSound();
+        if(playerSystem.isAttck)
+        {
+            m_AttackTrail.SetActive(true);
+            m_AttckCol.SetActive(true);
+        }
+        if (playerSystem.isStrongAttck)
+        {
+            m_PAAttackCol.SetActive(true);
+            m_PWAtackTrail.SetActive(true);
+        }
     }
 
     private void MeleeAttckEnd()
     {
+        m_PAAttackCol.SetActive(false);
         m_AttckCol.SetActive(false);
         m_AttackTrail.SetActive(false);
+        m_PWAtackTrail.SetActive(false);
         EndAttckAnimation();
+        PlayerSystem playerSystem = GetComponentInParent<PlayerSystem>();
+        playerSystem.isAttck = false;
+        playerSystem.isStrongAttck = false;
 
     }
     private void EndAttckAnimation()
@@ -50,6 +78,16 @@ public class PlayerMeleeWepon : MonoBehaviour
         m_PlayerAnimator.SetBool("StrongAttack", false);
         m_PlayerAnimator.SetBool("SecondNomalAttck", false);
         m_PlayerAnimator.SetBool("ThirdNomalAttck", false);
+    }
+    private void AttckSound()
+    {
+        if (m_ATVoices != null && m_ATVoices.Count > 0)
+        {
+            int voiceRandomIndex = Random.Range(0, m_ATVoices.Count); // ランダムなインデックスを取得
+            AudioClip selectedVoice = m_ATVoices[voiceRandomIndex]; // ランダムに選択された攻撃ボイスを取得
 
+            m_Voice.clip = selectedVoice;
+            m_Voice.Play();
+        }
     }
 }
